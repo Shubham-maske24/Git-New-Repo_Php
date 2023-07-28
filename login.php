99a4a318e2577a0d48a8aa534bf3638b3fbf8a24
Shubham Maske <shubham.maske24@gmail.com>
2023-07-28 09:03:05 +0000

    commiting .gitignore files

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
             $password = stripcslashes($password);  
             $username = mysqli_real_escape_string($con, $username);  
             $password = mysqli_real_escape_string($con, $password);  
-          
+            $password = mysqli_real_escape_string($con, $pass);
             $sql = "select *from login where username = '$username' and password = '$password'";  
             $result = mysqli_query($con, $sql);  
             $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
@@ -20,4 +20,4 @@
             else{  
                 echo "<h1> Login failed. Invalid username or password.</h1>";  
             }     
-    ?>  
+    ?> 
